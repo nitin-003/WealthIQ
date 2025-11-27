898fafef10fc409c2dbcfee95e0649a94fb1e5d6
Nitin Pathak <nitinpathak8242@gmail.com>
2025-11-27 16:42:50 +0530

    feat: add transaction page

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,9 +1,20 @@
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
 import React from "react";
+import AddTransactionForm from "../_components/transactionform";
 
-const page = () => {
-  return <div>Hello this is transaction create page</div>;
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
+
+      <AddTransactionForm accounts={accounts} categories={defaultCategories}/>
+    </div>
+  );
 };
 
-export default page;
+export default AddTransactionPage;
 
 
