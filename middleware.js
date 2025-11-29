47848a2a190c0f48925418e74598da021c09cddf
Nitin Pathak <nitinpathak8242@gmail.com>
2025-11-29 22:06:46 +0530

    fix: reduce Edge middleware size by removing Arcjet from middleware.js

--- middleware.js
+++ middleware.js
@@ -1,31 +1,21 @@
-import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server/clerkMiddleware";
+import { createRouteMatcher } from "@clerk/nextjs/server/createRouteMatcher";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/account(.*)", "/transaction(.*)"]);
-
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
-    }),
-  ],
-});
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)"
+]);
 
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
-
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
 });
 
-export default createMiddleware(aj, clerk);
+export default clerk;
 
 export const config = {
   matcher: [
