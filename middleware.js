3c001505317f9033ee4a3bc5307726c20983e4b1
Nitin Pathak <nitinpathak8242@gmail.com>
2025-11-29 16:17:33 +0530

    Fix Edge middleware size by excluding Arcjet

--- middleware.js
+++ middleware.js
@@ -1,17 +1,25 @@
-import { createMiddleware } from "@arcjet/next";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { aj } from "@/lib/arcjet-middleware";   
+import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/account(.*)", "/transaction(.*)"]);
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
 
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
-  if (!userId && isProtectedRoute(req)) {
+  if(!userId && isProtectedRoute(req)){
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
@@ -21,9 +29,11 @@ export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
-
 
